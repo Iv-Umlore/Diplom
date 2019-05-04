da81e075f426bc8557a6ca4d27104173b8ebd16a
Iv-Umlore <iv.kiselev1998@yandex.ru>
2019-05-04 23:02:46 +0300

    First step

---
+++ Client/Client/Speaker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace SocketTcpClient
+{
+    class Speaker
+    {
+        // адрес и порт сервера, к которому будем подключаться
+        static int port = 1024; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+        static public void Send(string args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // подключаемся к удаленному хосту
+                socket.Connect(ipPoint);
+                Console.Write("Введите сообщение:");
+                byte[] data = Encoding.Unicode.GetBytes(args);
+                socket.Send(data);
+
+                // получаем ответ
+                data = new byte[256]; // буфер для ответа
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; // количество полученных байт
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                Console.WriteLine("\n\n");
+                Console.WriteLine("ответ сервера: " + builder.ToString());
+
+                // закрываем сокет
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+
+    }
+}
+
